d7479873a0869680f0e20ff53c231d3ca8b13c67
Matheus S. Pereira <matheussilva.fga@gmail.com>
2016-09-12 17:27:21 -0300

    Criando função exponenciação

--- main.c
+++ main.c
@@ -1,11 +1,26 @@
 #include <stdio.h>
+#include <math.h>
 
 int soma(int a,int b){
 	return a+b;
 }
 
+int exponenciacao(int a, int b){
+	int valor = pow(a,b);
+
+	return valor;
+}
+
+
 int main (){
-	int a=1,b=2;
-	printf("Soma = %d\n",soma(a,b));
+
+ int a, b, valor;
+
+ a = 2;
+ b = 2;
+
+ valor = exponenciacao(a, b);
+ printf("%d\n", valor);
+
 
 }
